0afccae067b6076618ffa3e7537ed176d07f387b
marien272 <marien.olympio@epitech.eu>
2026-02-20 01:52:26 -0500

    partie4 sha256

--- lab_udp/client.py
+++ lab_udp/client.py
@@ -1,4 +1,5 @@
 import socket
+import hashlib
 from pathlib import Path
 
 HOST = "127.0.0.1"
@@ -6,7 +7,10 @@ PORT = 12345
 
 msg = Path("data/message.txt").read_text(encoding="utf-8").encode("utf-8")
 
+h = hashlib.sha256(msg).hexdigest()         # str 64 caractères hex
+payload = msg + b"\x00" + h.encode("ascii") # bytes
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.sendto(msg, (HOST, PORT))
+    s.sendto(payload, (HOST, PORT))
     data, _ = s.recvfrom(64)
     print("Réponse:", data.decode("utf-8", errors="replace"))
--- lab_udp/server.py
+++ lab_udp/server.py
@@ -1,4 +1,5 @@
 import socket
+import hashlib
 
 HOST = "127.0.0.1"
 PORT = 12345
@@ -9,5 +10,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     while True:
         data, addr = s.recvfrom(65535)
         print(f"Reçu {len(data)} octets de {addr}")
-        print(f"Message : {data.decode('utf-8', errors='replace')}")
-        s.sendto(b"Message recu", addr)
+
+        message, hash_hex = data.split(b"\x00", 1)
+        calc = hashlib.sha256(message).hexdigest().encode("ascii")
+
+        if calc == hash_hex:
+            print("Hash valide ✓")
+            s.sendto(b"Message et hachage valides", addr)
+        else:
+            print("Hash invalide ✗")
+            s.sendto(b"Erreur de hachage", addr)
